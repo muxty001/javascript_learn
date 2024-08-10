f8a41e36de303be4def13a5cc5ef4919c94e822b
unknown <muxty001-github>
2024-08-10 16:26:48 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+// DOM Manipulation
+
+
+
+// GetElementById()
+
+/* const title = document.getElementById('main-heading');
+
+ console.log(title);
+ */
+
+
+// GetElementByClassName()
+
+//const listItems = document.getElementsByClassName('list-items')
+
+//console.log(listItem);
+
+
+ // GetElementByTagName()
+ const listItems = document.getElementsByTagName('li');
+
+console.log(listItems);
+
+
+// querySelector()
+
+const container = document.querySelector('div');
+
+console.log(container);
+
+
+// querySelectorAll()
+
+
+
